29b57425668ec39b30bcaa85370d1b175f399324
Wesley Liao <wes@wesliao.com>
2020-07-29 15:14:20 -0400

    renaming

--- falcon_c_test.py
+++ falcon_c_test.py
@@ -2,24 +2,25 @@
 
 import ctypes
 
-ctypes.cdll.LoadLibrary("./lib/libnifalcon_c.so")
+ctypes.cdll.LoadLibrary("./lib/falcon_c.so")
 
-falcon_py = ctypes.CDLL("./lib/libnifalcon_c.so")
+falcon_c = ctypes.CDLL("./lib/falcon_c.so")
 
-falcon_ref = ctypes.c_void_p()
-falcon_ref = falcon_py.falcon_init(0)
+falcon_ref = falcon_c.falcon_init(0)
 
-error = falcon_py.falcon_load_firmware(falcon_ref, "firmware/test_firmware.bin")
+error = falcon_c.falcon_load_firmware(falcon_ref, "firmware/test_firmware.bin")
 
 print(error)
 
-get_pos_x = falcon_py.falcon_get_pos_x
+
+get_pos_x = falcon_c.falcon_get_pos_x
+# Must set the return type of the position functions to double or they will be cast to int
 get_pos_x.restype = ctypes.c_double
 
 for i in range(10000):
-    falcon_py.falcon_run_io_loop(falcon_ref)
+    falcon_c.falcon_run_io_loop(falcon_ref)
     x = get_pos_x(falcon_ref)
     print(x)
 
 
-falcon_py.falcon_exit(falcon_ref)
+falcon_c.falcon_exit(falcon_ref)
